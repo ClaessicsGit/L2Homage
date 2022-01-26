942aa690edaaa7a50f2ba3b164e6be62a6932e72
Claessic <Claessic88@gmail.com>
2022-01-25 23:18:41 -0800

    Fixed npcstring decryption

--- L2Homage/Tools/mxencdec/decrypt_npcstring-e.bat
+++ L2Homage/Tools/mxencdec/decrypt_npcstring-e.bat
@@ -3,6 +3,6 @@ echo f | xcopy /s /f "%~dp0..\..\Data\encrypted client files\npcstring-e.dat" np
 echo x|mxencdec.exe npcstring-e.dat
 
 "%~dp0l2asm-disasm\l2disasm.exe" -d "%~dp0l2asm-disasm\dats\npcstring-e.ddf" -e "%~dp0l2asm-disasm\newdats\npcstring-e-new.ddf" "npcstring-e.dat" "%~dp0..\..\Data\decrypted client files\npcstring-e.txt"
-del npcgrp.dat /f /q
+del npcstring-e.dat /f /q
 
 exit
